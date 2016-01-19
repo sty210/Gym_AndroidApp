a5925d92af20b382982e3f7a4d5630c11fb5bc1d
TaeYang Song <tysong@woowahan.com>
2016-01-19 16:27:36 +0900

    첫번째 커밋

---
+++ Gym_Android/app/src/main/java/com/baemin/sun/gym_android/ExerciseExplanation.java
@@ -0,0 +1,14 @@
+package com.baemin.sun.gym_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseExplanation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_explanation);
+
+    }
+}
---
+++ Gym_Android/app/src/main/java/com/baemin/sun/gym_android/Reviews.java
@@ -0,0 +1,13 @@
+package com.baemin.sun.gym_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+    }
+}
